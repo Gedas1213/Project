92237b871614b2a93a183063118c3a872d960720
Gedas1213 <121048673+Gedas1213@users.noreply.github.com>
2023-05-08 16:14:07 +0300

    Delete BHSL directory

--- BHSL/BHSLFX/forms.py
+++
@@ -1,8 +0,0 @@
-from .models import UserAccount
-from django import forms
-
-class AccountCreateForm(forms.ModelForm):
-    class Meta:
-        model = UserAccount
-        fields = ('account_number', 'account_password', 'server')
-        widgets = {'owner': forms.HiddenInput()}
